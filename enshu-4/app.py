3a72decdb03a544709ea8f170e21c662742b0114
yoppy_yophy <yopyopyopyophy@gmail.com>
2021-12-27 00:40:49 +0900

    first commit

---
+++ enshu-4/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return '<h1>こんにちは</h1>'
+
+
+@app.route('/hello/<string:name1>/<string:name2>')
+def show_name(name1, name2):
+    return '<h1>こんにちは{}さん{}さん</h1>'.format(name1, name2)
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add_num(num1, num2):
+    return '{}'.format(num1 + num2)
+
+
+@app.route('/div/<float:num1>/<float:num2>')
+def div_num(num1, num2):
+    return '{}'.format(num1 // num2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
